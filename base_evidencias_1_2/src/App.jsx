e6c39b4f26cdec649edf4b6721e85a64af2a057e
PereyraHebe <p_hebe_c@hotmail.com>
2025-05-19 17:11:43 -0300

    Se agrega la funcion del buscador con renderizacion condicional

--- base_evidencias_1_2/src/App.jsx
+++ base_evidencias_1_2/src/App.jsx
@@ -1,29 +1,44 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import TarjetaProducto from "./assets/componentes/TarjetaProducto";
 import "./App.css";
+//impotacion de componentes
+import TarjetaProducto from "./assets/componentes/TarjetaProducto";
+
 
 function App() {
   const [productos, setProductos] = useState([]);
+  const [buscador, setBuscador] = useState("");
 
   useEffect(() => {
-    axios.get("https://dummyjson.com/products").then((res) => {
+    axios.get("https://dummyjson.com/products?limit=180").then((res) => {
       setProductos(res.data.products);
     });
   }, []);
 
+  //filtracion de productos 
+  const productosFiltrados= productos.filter((p)=>p.title.toLowerCase().includes(buscador.toLowerCase()))
+
   return (
-    <div className="p-6">
+    < div className="p-6">
       <h1 className="text-2xl font-bold text-center mb-4">- Proyecto Integrador - Evaluación 1 y 2</h1>
       
       <h2 className="text-xl font-semibold mb-2">Productos de Dummyjson.com</h2>
+
+      <input type="text" placeholder="Busca tu producto aqui" value={buscador} 
+      onChange={(e)=>{setBuscador(e.target.value)}}/>
       
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {productos.map((p) => (
+        {productosFiltrados.map ((p) => (
           <TarjetaProducto key={p.id} producto={p} />
         ))}
       </div>
+      {/* Renderizacion condicional*/}
+      {productosFiltrados.length === 0 && <div className="p-4"> No se encontraron productos</div>}
+      
+
+      
     </div>
+    
   );
 }
 
